ba9e47abd7f878639d6070418e51d8a9451c2c1f
Xiaosong Bi <bixiaosong@gmail.com>
2024-02-26 16:59:54 +0100

    Initialize project: The source project code has added MongoDB Atlas cloud username and password and successfully tested the connection.

---
+++ server/database/connection.js
@@ -0,0 +1,21 @@
+// connection.js
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config({ path: '../.env' })
+
+const databaseUrl =
+  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+
+module.exports = async () => {
+  try {
+    await mongoose.connect(databaseUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    console.log('Database successfully connected')
+  } catch (error) {
+    console.error(`Database Connectivity Error: ${error}`)
+    throw new Error(error)
+  }
+}
