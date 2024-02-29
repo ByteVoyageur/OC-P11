2eeba5b5fe1baa5071d882b3f19bf3f3d0a0079a
Xiaosong Bi <bixiaosong@gmail.com>
2024-02-29 14:50:38 +0100

    Added the logic for modifying userName in the FormEditName component and successfully tested it.

--- src/components/FormEditName/index.js
+++ src/components/FormEditName/index.js
@@ -1,16 +1,30 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setUserName } from '../../redux/userActions' // import the action creator
+import axios from 'axios' // import axios
 
 function FormEditName() {
   const dispatch = useDispatch()
-  const userName = useSelector((state) => state.user.userName) // access userName from Redux state
-  const [newUserName, setNewUserName] = useState('') // local state for new username
+  const userName = useSelector((state) => state.user.userName) // get current userName from Redux state
+  const token = useSelector((state) => state.user.token?.token) // get token from Redux state
+  const [newUserName, setNewUserName] = useState(userName) // initialize newUserName with current userName
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    dispatch(setUserName(newUserName)) // dispatch action to update userName
+    try {
+      const response = await axios.put(
+        'http://localhost:3001/api/v1/user/profile',
+        { userName: newUserName },
+        { headers: { Authorization: `Bearer ${token}` } }
+      ) // send PUT request to API
+      if (response.status === 200) {
+        dispatch(setUserName(newUserName)) // dispatch action to update userName
+      }
+    } catch (error) {
+      console.error(error)
+    }
   }
+
   console.log('formEditName组件渲染前把userName显示在这里:', userName)
   return (
     <form onSubmit={handleSubmit}>
