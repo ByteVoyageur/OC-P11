8e47803c6ffd5284c1c47ec9d894028ce01128af
Xiaosong Bi <bixiaosong@gmail.com>
2024-03-20 19:31:24 +0100

    Backup after deleting DockerBackup directory, preparing to use git rebase to drop DockerBackup directory

--- src/pages/UserEdit/index.js
+++ src/pages/UserEdit/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import getToken from '../../hooks/getToken'
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
 import { setFirstName, logOut } from '../../redux/userActions'
