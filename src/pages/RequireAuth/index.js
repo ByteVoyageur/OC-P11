5b0d4bdde48dfd01c84a79a6be42d6b41eac6dad
Xiaosong Bi <bixiaosong@gmail.com>
2024-03-20 14:46:03 +0100

    Successfully deployed the project to banque.xiaosong.fr, preparing to synchronize local project code with the server code as the standard. This commit serves as a backup before synchronization.

--- src/pages/RequireAuth/index.js
+++ src/pages/RequireAuth/index.js
@@ -1,3 +1,4 @@
+// RequireAuth component
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -23,8 +25,12 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'public', to: '.' }],
     }),
   ],
   resolve: {
